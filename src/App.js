07b70d5d49e59555e0bd44bd3c2edb91045c7153
juniorgianisini <juniorgianisini@gmail.com>
2019-02-06 01:21:50 -0200

    fix: Removido propriedade não utilizada

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import BookSearch from './BookSearch'
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
 
   componentDidMount() {
@@ -27,11 +20,8 @@ class BooksApp extends React.Component {
   }
 
   render() {
-    //<button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-    // <Link to="/search">Add a book</Link>
     return (
-
-      <div className="app">
+    <div className="app">
         <Route exact path="/" render={() => (<div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
